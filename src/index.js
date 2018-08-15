0c3a3fa6f8e31deee6216d6a739b46e04f23d741
charlesyn <charlesyniu@gmail.com>
2018-08-15 13:55:59 -0400

    add logic for showing users in same room

--- src/index.js
+++ src/index.js
@@ -14,16 +14,26 @@ routes(app)
 io.on('connection', function (socket) {
   let connectedClients = Object.keys(io.of('/').clients().connected)
   let currentRoom = ''
+  let username = ''
   console.log(connectedClients)
   socket.on('room', function (roomInfo) {
-    console.log(roomInfo.username + ' has joined ' + roomInfo.room)
+    username = roomInfo.username
+    socket.broadcast.to(currentRoom).emit('removeUser', username)
+    socket.emit('clearUserList')
     socket.join(roomInfo.room)
     currentRoom = roomInfo.room
+    socket.broadcast.to(currentRoom).emit('pollUsers', socket.id)
+    socket.broadcast.to(currentRoom).emit('updateUserList', roomInfo.username)
+  })
+  socket.on('getUserList', function (data) {
+    console.log('Recipient: ' + data.recipient)
+    io.to(data.recipient).emit('updateUserList', data.username)
   })
   socket.on('draw', function (data) {
     socket.broadcast.to(currentRoom).emit('draw', data)
   })
   socket.on('disconnect', function () {
+    socket.broadcast.to(currentRoom).emit('removeUser', username)
     console.log('A user disconnected')
   })
 })
